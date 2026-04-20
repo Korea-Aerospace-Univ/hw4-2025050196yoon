0b587b6ec3ca90306d9501a47312a81238a56cf6
2025050196yoon <choyoon@kau.kr>
2026-04-20 16:01:45 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+    int N ;
+    scanf("%d\n", &N);
+    
+    int cntLower = 0, cntDigit = 0, maxLower = 0, maxDigit =0;
+    
+    for ( int i =0; i < N; i++) {
+        char c;
+        scanf("%c", &c);
+        
+        if (c >= 'a' && c <= 'z') {
+            cntLower++;
+            if (cntLower > maxLower) maxLower = cntLower;
+        } else {
+            cntLower = 0;
+        }
+        
+        if (c >= '0' && c <= '9') {
+            cntDigit++;
+            if (cntDigit > maxDigit) maxDigit = cntDigit;
+        } else {
+            cntDigit = 0;
+        } 
+    } 
+    
+    printf("%d\n%d\n", maxLower, maxDigit);
+    
+    return 0;
+}
