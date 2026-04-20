06c951f541d9a650b9dea3417921a7857dd75810
2025050196yoon <choyoon@kau.kr>
2026-04-20 16:42:11 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -9,8 +9,7 @@ int main(void) {
     
     for ( int i =0; i < N; i++) {
         char c;
-        getchar();
-        scanf("%c", &c);
+        scanf(" %c", &c);
         
         if (c >= 'a' && c <= 'z') {
             cntLower++;
