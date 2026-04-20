2eef1ebdbec8430658c6e287b1950f5a35b4d0e9
2025050196yoon <choyoon@kau.kr>
2026-04-20 15:36:40 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,25 +3,25 @@
 int main(void) {
     int answer, guess, count = 0;
     
-    scanf("%d ", &answer);
+    scanf("%d", &answer);
     
     do { 
         scanf("%d", &guess);
         count++;
         
         if (guess > answer) {
-            printf("%d > ?\n", guess);
+            printf("%d>?\n", guess);
         }
         else if ( guess < answer) {
-            printf("%d < ?\n", guess);
+            printf("%d<?\n", guess);
         }
         else { 
-            printf("%d == ?\n", guess);
+            printf("%d==?\n", guess);
         }
         
     }while (guess != answer);
     
-    printf("%d\n", count);
+    printf("%d", count);
     
     return 0;
     
